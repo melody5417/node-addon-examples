ecee321643f8d586c3392de24a962ce035f605e4
Melody <melody5417@users.noreply.github.com>
2020-11-11 20:03:09 +0800

    Update hello.cc

--- 1_hello_world/node-addon-api/hello.cc
+++ 1_hello_world/node-addon-api/hello.cc
@@ -1,3 +1,6 @@
+// node-addon-api: 
+// header-only C++ wrapper classes which simplify the use of the C-based N-API.
+
 #include <napi.h>
 
 Napi::String Method(const Napi::CallbackInfo& info) {
@@ -11,4 +14,6 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   return exports;
 }
 
+// 所有的 Node.js 插件必须导出一个如下模式的初始化函数
+// hello 是最终的二进制名， Init 是初始化函数名
 NODE_API_MODULE(hello, Init)
